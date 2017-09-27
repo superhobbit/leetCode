669d23cfdb1747b3bf0f43aba551cfbbe806dc9d
superhobbit <13071227043@163.com>
2017-09-26 23:12:08 -0400

    Create 3_Longest_Substring_Without_Repeating_Characters.cpp

---
+++ 3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,25 @@
+Given a string, find the length of the longest substring without repeating characters.
+
+Examples:
+
+Given "abcabcbb", the answer is "abc", which the length is 3.
+
+Given "bbbbb", the answer is "b", with the length of 1.
+
+Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+int lengthOfLongestSubstring(string s) {
+    if (s.length() == 0) return 0;
+    map<char, int> total;
+    int j = 0, maxI = 0;
+    for (int i = 0; i < s.length(); i++){
+        if (total.find(s[i])!=total.end()){
+            j = std::max(j,total.find(s[i])->second+1);
+            total.erase(total.find(s[i]));
+        }
+        total.insert(pair<char,int>(s[i],i));
+        maxI = std::max(maxI,i-j+1);
+    }
+    return maxI;
+}
