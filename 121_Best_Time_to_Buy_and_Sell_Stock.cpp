29b2129c0dc463c6215a3632e52cb75b45207e67
superhobbit <13071227043@163.com>
2017-10-03 22:20:20 -0400

    Create 121_Best_Time_to_Buy_and_Sell_Stock.cpp

---
+++ 121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,27 @@
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Example 1:
+Input: [7, 1, 5, 3, 6, 4]
+Output: 5
+
+max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+Example 2:
+Input: [7, 6, 4, 3, 1]
+Output: 0
+
+In this case, no transaction is done, i.e. max profit = 0.
+
+
+!!!First time come up with a neatly solution!!! Though it is easy==But still!!!
+int maxProfit(vector<int>& prices) {
+    if (prices.size() == 0 || prices.size() == 1) return 0;
+    int lower = INT_MAX, max_pro = 0;
+    for (int i = 0; i < prices.size(); ++i){
+        lower = min(lower,prices[i]);
+        max_pro = max(max_pro,prices[i]-lower);
+    }
+    if (max_pro <= 0) return 0;
+    return max_pro;
+}
