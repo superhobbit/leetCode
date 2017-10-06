1a2a2351eb93ad791b38ddfcb3eeffae02aa30f5
superhobbit <13071227043@163.com>
2017-10-06 00:43:19 -0400

    Create 56_Merge_Intervals.cpp

---
+++ 56_Merge_Intervals.cpp
@@ -0,0 +1,26 @@
+Given a collection of intervals, merge all overlapping intervals.
+
+For example,
+Given [1,3],[2,6],[8,10],[15,18],
+return [1,6],[8,10],[15,18].
+
+Erase 还是能不用就不用吧
+vector<Interval> merge(vector<Interval>& intervals) {
+    if (intervals.size()<2) return intervals;
+    std::sort(intervals.begin(),intervals.end(),[] (Interval & a, Interval & b) { return a.start < b.start; });
+    for(int i = 1; i <= intervals.size()-1; ++i){
+        if (intervals[i-1].end >= intervals[i].start){
+            if (intervals[i-1].end <= intervals[i].end) {
+                intervals[i-1].end = intervals[i].end;
+                intervals.erase(intervals.begin()+i);
+                --i;
+            }else{
+                intervals.erase(intervals.begin()+i);
+                --i;
+            }
+        }
+    }
+    vector<Interval> result;
+    result.assign(intervals.begin(), intervals.end());
+    return result;
+}
