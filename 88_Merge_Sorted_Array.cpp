7de92bc790b89ba4d1efb9c6a0472a2e73452afd
superhobbit <13071227043@163.com>
2017-10-09 21:38:07 -0400

    Update 88_Merge_Sorted_Array.cpp

--- 88_Merge_Sorted_Array.cpp
+++ 88_Merge_Sorted_Array.cpp
@@ -22,3 +22,12 @@ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 
     nums1 = result;
 }
+
+
+A more elegant method in the answers:
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i = m+n-1, j = m-1, k = n-1;
+    while(k >= 0 && i >= 0){
+        nums1[i--] = nums1[j] > nums2[k] && j >= 0 ? nums1[j--] : nums2[k--];
+    }
+}
