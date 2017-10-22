1f3c761f7b96566f2c2c7a5c1f2e24e0d266d5ad
superhobbit <13071227043@163.com>
2017-10-21 20:08:38 -0400

    Create 647_Palindromic_Substrings.cpp

---
+++ 647_Palindromic_Substrings.cpp
@@ -0,0 +1,29 @@
+Given a string, your task is to count how many palindromic substrings in this string.
+
+The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+
+Example 1:
+Input: "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+Input: "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+
+
+DP solution:
+    int countSubstrings(string s) {
+        vector<vector<int>>dp(s.size()+1,vector<int>(s.size(),0));
+        int count = 0;
+        for (int i = 0; i < s.size(); ++i){
+            for (int j = i; j >= 0; --j){
+                dp[j][i] = (s.at(i) == s.at(j) && (i-j < 2 || dp[j+1][i-1]));
+                if (dp[j][i]) count++;
+            }
+        }
+        return count;
+    }
+    
+    
+General solution: use single element as center, and find odd and even results seperetly:
